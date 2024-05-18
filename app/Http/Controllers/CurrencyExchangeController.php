639aaf08298b65330265a77d73c21f7e9e4c84f4
zeqas <casperi52400@gmail.com>
2024-05-18 17:54:12 +0800

    refactor: 將邏輯放在 Service 內

--- app/Http/Controllers/CurrencyExchangeController.php
+++ app/Http/Controllers/CurrencyExchangeController.php
@@ -26,26 +26,8 @@ class CurrencyExchangeController extends Controller
         $target = (string) $request->input('target');
         $amount = $request->input('amount');
 
-        // 驗證參數
-
-        // source、target 必須為 TWD, JPY, USD 三種貨幣之一
-        if (!$this->currencyExchangeService->isValidCurrency($source) ||        !$this->currencyExchangeService->isValidCurrency($target)) {
-            return response()->json([
-                "message" => "必須為有效的貨幣ISO代碼",
-            ]);
-        }
-
-        // amount 輸入時無論有無千分位皆可接受。例如「1,525」或「1525」皆可。
-        $amountWithoutCommas = str_replace(',', '', $amount);
-
-        if (!is_numeric($amountWithoutCommas)) {
-            return response()->json([
-                "message" => "必須為有效數字",
-            ]);
-        }
-
         // 呼叫 convert 方法
-        $convertedAmount = $this->currencyExchangeService->convert($source, $target, $amountWithoutCommas);
+        $convertedAmount = $this->currencyExchangeService->convert($source, $target, $amount);
 
         return response()->json([
             "message" => "success",
--- app/Services/CurrencyExchangeService.php
+++ app/Services/CurrencyExchangeService.php
@@ -17,10 +17,25 @@ class CurrencyExchangeService
     // 匯率轉換
     public function convert($source, $target, $amount)
     {
-        // 如果 amount 中含有逗號，則移除逗號
-        $amount = str_replace(',', '', $amount);
+        // 驗證參數
 
-        $rawConvertedAmount = $amount * $this->currency_rates[$source][$target];
+        // source、target 必須為 TWD, JPY, USD 三種貨幣之一
+        if (!$this->isValidCurrency($source) || !$this->isValidCurrency($target)) {
+            return response()->json([
+                "message" => "必須為有效的貨幣ISO代碼",
+            ]);
+        }
+
+        // amount 輸入時無論有無千分位皆可接受。例如「1,525」或「1525」皆可。
+        $amountWithoutCommas = str_replace(',', '', $amount);
+
+        if (!is_numeric($amountWithoutCommas)) {
+            return response()->json([
+                "message" => "必須為有效數字",
+            ]);
+        }
+
+        $rawConvertedAmount = $amountWithoutCommas * $this->currency_rates[$source][$target];
         // 四捨五入到小數點第二位
         $roundedConvertedAmount = round($rawConvertedAmount, 2);
         // 加上半形逗點作為千分位表示，每三個位數一點
@@ -35,12 +50,4 @@ class CurrencyExchangeService
     {
         return array_key_exists($currency, $this->currency_rates);
     }
-
-    // TODO 測試案例
-    /*
-     *  CurrencyExchangeService 必須包含（但不限於）以下測試案例：
-     *  1. 若輸入的 source 或 target 系統並不提供時的案例
-     *  2. 若輸入的金額為非數字或無法辨認時的案例
-     *  3. 輸入的數字需四捨五入到小數點第二位，並請提供覆蓋有小數與沒有小數的多種案例
-    */
 }
